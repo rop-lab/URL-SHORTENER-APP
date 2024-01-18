4aa83cc914d6de378407d1ccac29e6b83c5d3e33
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-18 17:58:41 +0300

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    // get references to html elements
+    const shortenForm = document.getElementById('shortenForm');
+    const shortenedurlContainer = document.getElementById('shortenedUrlContainer');
+    const likeButton = document.getElementById('likeButton');
+
+    // variable to store the current URL information
+    let currentUrl;
+    
+
+})
