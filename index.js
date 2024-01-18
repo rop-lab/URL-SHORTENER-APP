6ace7e63cc9dbc9a42a09719b63d756871d64aa6
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-18 18:43:11 +0300

    commit

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     // variable to store the current URL information
     let currentUrl;
-    
 
+shortenForm.addEventListener('submit', async (e) =>{
+    e.preventDefault();
+    const originalUrl = document.getElementById('originalUrl').value;
+   try{
+    const response = await fetch('db.json');
+    const data = await response.json();
+    const existingUrl = data.urls.find(url => url.originalUrl === originalUrl)
+    if(existingUrl){
+        currentUrl = existingUrl;
+    } else {
+        currentUrl = {
+            id: data.urls.length + 1,
+            originalUrl: originalUrl
+            shortenedUrl: `https://yourshortenedurl.com/${data.urls.length + 1}`,
+            likes: 0
+        };
+        data.urls.push(currentUrl);
+    }
+    }
+   } 
+})
 })
